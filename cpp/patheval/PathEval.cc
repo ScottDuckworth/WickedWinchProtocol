6892fd65e90dd4a81259f9fb5a4598d481b2d857
Scott Duckworth <scott.duckworth@gmail.com>
2024-07-04 21:04:13 -0700

    Create patheval library

---
+++ cpp/patheval/PathEval.cc
@@ -0,0 +1,27 @@
+#include "PathEval.h"
+#include "proto/PathExpression.pb.h"
+
+#include <algorithm>
+
+namespace wickedwinch::patheval {
+
+PathEval PathAt(const wickedwinch::proto::Path& path, uint32_t t) {
+  struct AtCompare {
+    bool operator()(uint32_t t, const wickedwinch::proto::PathSegment& segment) const {
+      return t < segment.start_time();
+    }
+  };
+  const auto& segments = path.segments();
+  auto it = std::upper_bound(segments.begin(), segments.end(), t, AtCompare());
+  if (it == segments.begin()) return {nullptr, 0};
+  --it;
+  return {&*it, t - it->start_time()};
+}
+
+wickedwinch::postfix::EvalStatus PathEval::Eval(std::vector<float>& stack) const {
+  stack.clear();
+  stack.push_back(seconds());
+  return postfix::Eval(segment->expr(), stack);
+}
+
+}
